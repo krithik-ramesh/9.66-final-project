098f3908f41026d7aac4050cda83904abbfd6b86
mna1024 <maxiattiogbe@gmail.com>
2023-12-18 08:55:22 -0500

    Remove dead code

--- GPT-3.5-WebPPL/test.py
+++ GPT-3.5-WebPPL/test.py
@@ -12,7 +12,6 @@ client = OpenAI(api_key=my_api_key)
 
 directory = ["/Users/maximattiogbe/Desktop/9.66-final-project/GPT-3.5-WebPPL/bAbI_tasks_1-20_v1-2/en/qa1_single-supporting-fact_test.txt"]
 
-# queries = ["All humans are mortal. Socrates is a human.\nWhat can you conclude about Socrates?"]
 queries = []
 answers = []
 
@@ -23,32 +22,18 @@ for file_path in directory:
         query = ""
         for line in file:
             query += " ".join(line.strip().split()[1:]) + " "
-            # print(f"QUERY = {query}")
             line_cnt += 1
 
             if line_cnt % 3 == 0:
                 end = query.index("?")
                 answer = query[end + 1:].split()[0]
                 query = query[:end + 1]
-                # print(f"QUERY = {query}")
-                # print(f"ANSWER = {answer}")
-                # print()
 
                 queries.append(query)
                 answers.append(answer)
-                # print(f"QUERIES = {queries}")
-                # print(f"ANSWERS = {answers}")
-                # print()
-                # print()
 
                 query = ""
                 answer = ""
-
-                # if line_cnt == 30:
-                #     break
-
-
-failure_cnt = 0
 
 for query in queries:
     response = client.chat.completions.create(
@@ -60,6 +45,3 @@ for query in queries:
     ]
     )
     print(response.choices[0].message.content)
-    if response.choices[0].message.content.value.lower() == "unknown" or len(response.choices[0].message.content.value.split()) > 1:
-        failure_cnt += 1
-    print(f"{failure_cnt} FAILURES") 
